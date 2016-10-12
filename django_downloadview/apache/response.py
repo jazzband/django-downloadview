315bec54c35362e51f43d95bf66480889315102b
pkaczynski <pkaczyns@gmail.com>
2016-10-12 12:55:30 +0200

    Unquoting response
    xsendfile was complaining about not seeing path with quotes

--- django_downloadview/apache/response.py
+++ django_downloadview/apache/response.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Apache's specific responses."""
 import os.path
+import six.moves.urllib as urllib
 
 from django_downloadview.response import (ProxiedDownloadResponse,
                                           content_disposition)
@@ -12,7 +13,8 @@ class XSendfileResponse(ProxiedDownloadResponse):
                  attachment=True):
         """Return a HttpResponse with headers for Apache X-Sendfile."""
         super(XSendfileResponse, self).__init__(content_type=content_type)
+
         if attachment:
             self.basename = basename or os.path.basename(file_path)
             self['Content-Disposition'] = content_disposition(self.basename)
-        self['X-Sendfile'] = file_path
+        self['X-Sendfile'] = urllib.parse.unquote(file_path)
