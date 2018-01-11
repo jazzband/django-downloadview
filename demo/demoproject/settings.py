169145bec7a1a21bf2872b2d80d63751f28ae1a3
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-01-11 16:02:54 +0100

    Avoid duplication of middleware setting

--- demo/demoproject/settings.py
+++ demo/demoproject/settings.py
@@ -64,24 +64,16 @@ INSTALLED_APPS = (
 
 
 # BEGIN middlewares
+MIDDLEWARE_CLASSES = [
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django_downloadview.SmartDownloadMiddleware'
+]
 if StrictVersion(get_version()) >= StrictVersion('1.10'):
-    MIDDLEWARE = [
-        'django.middleware.common.CommonMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django_downloadview.SmartDownloadMiddleware'
-    ]
-else:
-    MIDDLEWARE_CLASSES = [
-        'django.middleware.common.CommonMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django_downloadview.SmartDownloadMiddleware'
-    ]
+    MIDDLEWARE = MIDDLEWARE_CLASSES
 # END middlewares
 
 
